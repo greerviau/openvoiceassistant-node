f48d6ee91a16d9a908a6335fd3021e12beeef3ac
Greer <gviau2@gmail.com>
2022-08-08 22:25:08 -0400

    adding utils

---
+++ utils/hardware.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr 
+import sounddevice as sd
+
+def select_mic(mic_tag):
+    microphones = sr.Microphone.list_microphone_names()
+    try:
+        mic_index = [idx for idx, element in enumerate(microphones) if mic_tag in element.lower()][0]
+    except:
+        raise RuntimeError('Mic does not exist')
+    mic = microphones[mic_index]
+    print('Microphone: ', mic)
+    return mic_index, mic
+
+def get_samplerate(mic_index):
+    mic_info = sd.query_devices(mic_index, 'input')
+    samplerate = int(mic_info['default_samplerate'])
+    return samplerate
---
+++ utils/network.py
@@ -0,0 +1,41 @@
+import scapy.all as scapy
+import requests
+import socket
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+def get_subnet(ip):
+    return '.'.join(ip.split('.')[:2]) + '.0.0/24'
+
+def scan_for_hub(subnet, port):
+    while True:
+        devices = net_scan(subnet)
+        for device in devices:
+            ip = device['ip']
+            print('Testing ip ', ip)
+            try:
+                response = requests.get(f'http://{ip}:{port}/is_alive', timeout=5)
+                print('Hub Found')
+                return ip
+            except:
+                pass
+
+def net_scan(subnet):
+    arp_req_frame = scapy.ARP(pdst = subnet)
+
+    broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    
+    broadcast_ether_arp_req_frame = broadcast_ether_frame / arp_req_frame
+
+    answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
+    result = [{'ip': '127.0.0.1'}]
+    for i in range(0,len(answered_list)):
+        client_dict = {'ip' : answered_list[i][1].psrc}
+        result.append(client_dict)
+
+    return result
