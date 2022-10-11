4044c98fe5c55bc99c004c95a0fe36654b1e3b7a
greerviau <gviau2@gmail.com>
2022-10-11 01:37:48 -0400

    Working on noise reduction for vad

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-apt install -y python3-pip python3-venv python3-pyaudio git wget libatlas-base-dev libglib2.0-dev libgirepository1.0-dev libcairo2-dev
+apt install -y python3-pip python3-venv python3-pyaudio \
+                git wget libatlas-base-dev libglib2.0-dev \
+                libgirepository1.0-dev libcairo2-dev \
+                gfortran gcc libopenblas-dev libopenblas-base \
+                libblas-dev llvm python3-scipy
+
+python3 -m venv env/node
+
+source env/node/bin/activate
  
 pip3 install -r requirements.txt
--- node.py
+++ node.py
@@ -10,6 +10,8 @@ from playsound import playsound
 import time
 from io import BytesIO
 
+from utils import noisereduce
+
 
 class Node:
     def __init__(self, node_id, mic_index, hub_api_uri, debug):
@@ -28,24 +30,24 @@ class Node:
         self.INTERVAL = 30   # ms
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = 1
-        self.SAMPLEWIDTH = self.paudio.get_sample_size(self.FORMAT)
+        self.SAMPLE_WIDTH = self.paudio.get_sample_size(self.FORMAT)
 
         supported_rates = [48000, 32000, 16000, 8000]
-        self.RATE = None
+        self.SAMPLE_RATE = None
         for rate in supported_rates:
             if self.paudio.is_format_supported(rate, input_device=devinfo['index'], input_channels=self.CHANNELS, input_format=self.FORMAT):
-                self.RATE = rate
+                self.SAMPLE_RATE = rate
                 break
 
-        if self.RATE is None:
+        if self.SAMPLE_RATE is None:
             raise RuntimeError('Failed to set samplerate')
 
-        self.CHUNK = int(self.RATE * self.INTERVAL / 1000) 
+        self.CHUNK = int(self.SAMPLE_RATE * self.INTERVAL / 1000) 
 
         print('Mic Settings')
         print('Interval: ', self.INTERVAL)
         print('Channels: ', self.CHANNELS)
-        print('Samplerate: ', self.RATE)
+        print('Samplerate: ', self.SAMPLE_RATE)
         print('Chunk Size: ', self.CHUNK)
 
         self.running = True
@@ -62,7 +64,7 @@ class Node:
         stream = self.paudio.open(format=self.FORMAT,
                 channels=self.CHANNELS,
                 input_device_index = self.mic_index,
-                rate=self.RATE,
+                rate=self.SAMPLE_RATE,
                 input=True,
                 frames_per_buffer=self.CHUNK)
         
@@ -76,7 +78,11 @@ class Node:
         done = False
         while self.running:
             data = stream.read(self.CHUNK, exception_on_overflow=False)
-            if self.vad.is_speech(data, self.RATE):
+            audio_data = np.fromstring(data, dtype=np.int16)
+            reduce_noise = noisereduce.reduce_noise(y=audio_data, sr=self.SAMPLE_RATE)
+            is_speech = self.vad.is_speech(reduce_noise.tobytes(), self.SAMPLE_RATE)
+            print('Is speech: ', is_speech)
+            if is_speech:
                 self.__log('\rRecording...   ', end='')
                 voice_detected = True
                 if len(buffer) > 0:
@@ -107,8 +113,8 @@ class Node:
 
                     wf = wave.open('command.wav', 'wb')
                     wf.setnchannels(self.CHANNELS)
-                    wf.setsampwidth(self.SAMPLEWIDTH)
-                    wf.setframerate(self.RATE)
+                    wf.setsampwidth(self.SAMPLE_WIDTH)
+                    wf.setframerate(self.SAMPLE_RATE)
                     wf.writeframes(audio_data)
                     wf.close()
 
@@ -117,9 +123,11 @@ class Node:
                     time_sent = time.time()
 
                     payload = {
-                        'command_audio_data': raw_base64, 
+                        'command_audio_data_b64': raw_base64, 
+                        'command_audio_sample_rate': self.SAMPLE_RATE, 
+                        'command_audio_sample_width': self.SAMPLE_WIDTH, 
+                        'command_audio_channels': self.CHANNELS, 
                         'command_text': '',
-                        'command_sample_rate': self.RATE, 
                         'node_callback': '', 
                         'node_id': self.node_id, 
                         'engage': False,
@@ -162,12 +170,12 @@ class Node:
                             callout = context['callout']
 
                         self.__log(context['command'])
-                        response_audio_data = context['response_audio_data']
+                        response_audio_data_b64 = context['response_audio_data_b64']
                         response_sample_rate = context['response_sample_rate']
                         response_sample_width = context['response_sample_width']
                         print('Samplerate: ', response_sample_rate)
                         print('Samplewidth: ', response_sample_width)
-                        audio_bytes = base64.b64decode(response_audio_data)
+                        audio_bytes = base64.b64decode(response_audio_data_b64.encode('utf-8'))
 
                         audio_segment = pydub.AudioSegment(
                             audio_bytes, 
