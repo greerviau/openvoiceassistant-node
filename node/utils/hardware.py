2f715b782088b4b9ec076927ec2e78effdf01253
Greer <gviau2@gmail.com>
2023-03-22 13:08:03 -0400

    Update hardware.py

--- node/utils/hardware.py
+++ node/utils/hardware.py
@@ -62,7 +62,7 @@ def select_speaker(speaker: Union[str, int]) -> Tuple[int, str]:
         
         speaker_tag = speakers[speaker_index]
     except Exception as e:
-        raise RuntimeError(f'Microphone does not exist')
+        raise RuntimeError(f'Speaker does not exist')
         
     return speaker_index, speaker_tag
 
