8e9bf16c9333bb2101f975a6388d24c211cda7e0
Greer <gviau2@gmail.com>
2023-05-17 19:24:15 -0400

    Update listener.py

--- node/listener.py
+++ node/listener.py
@@ -86,6 +86,8 @@ class Listener:
                         # Speech in any chunk counts as speech
                         is_speech = is_speech or self.vad.is_speech(vad_chunk, self.sample_rate)
 
+                    print(is_speech)
+
                     if is_speech:
                         speech_end = 0
                     else:
