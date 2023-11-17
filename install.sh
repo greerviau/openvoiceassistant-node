8d0766f2db473853b562497818610dfe5c6825d8
Greer <gviau2@gmail.com>
2023-11-17 00:33:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 apt install -y python3-pip python3.11-venv python3-pyaudio \
                 git wget libatlas-base-dev libglib2.0-dev \
                 libgirepository1.0-dev libcairo2-dev \
-                gfortran gcc libopenblas-dev libopenblas-base \
+                gfortran gcc libopenblas-dev \
                 libblas-dev llvm python3-scipy build-essential sox
 
 CWD=$(pwd)
