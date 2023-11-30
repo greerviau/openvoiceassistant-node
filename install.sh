52bc6dc6e7ad95123cbd5a5287bff4fccc147f8b
Greer <gviau2@gmail.com>
2023-11-30 18:06:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-apt update 
-apt upgrade
+apt update
 apt install -y python3-pip python3.11-venv python3-pyaudio \
                 git wget libatlas-base-dev libglib2.0-dev \
                 libgirepository1.0-dev libcairo2-dev \
