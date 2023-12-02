4d40d455e5095397cbe7153e8ff5bc788c42cc17
Greer <gviau2@gmail.com>
2023-12-02 17:32:54 -0500

    Update audio_player.py

--- node/audio_player.py
+++ node/audio_player.py
@@ -7,16 +7,14 @@ class AudioPlayer:
     def __init__(self, node):
         self.node = node
         self.speaker_idx = node.speaker_idx
-        self.speaker_busy = False
+        self.speaker_busy = threading.Event()
 
     def play_audio_file(self, file: str, asynchronous: bool = False):
         def play_audio():
-            while self.speaker_busy:
-                time.sleep(0.1)
-            
-            self.speaker_busy = True
+            self.speaker_busy.wait()
+            self.speaker_busy.set()
             self.play_output_stream(file)
-            self.speaker_busy = False
+            self.speaker_busy.clear()
 
         if asynchronous:
             threading.Thread(target=play_audio, daemon=True).start()
@@ -30,20 +28,21 @@ class AudioPlayer:
 
     def play_output_stream(self, file):
         event = threading.Event()
-        def callback(outdata, frames, time, status):
-            data = wf.buffer_read(frames, dtype='float32')
-            if len(outdata) > len(data) or self.node.pause_flag.is_set():
-                outdata[:len(data)] = data
-                outdata[len(data):] = b'\x00' * (len(outdata) - len(data))
-                raise sd.CallbackStop
-            else:
-                outdata[:] = data
-
+        
         with sf.SoundFile(file) as wf:
-            stream = sd.RawOutputStream(samplerate=wf.samplerate,
-                                    channels=wf.channels,
-                                    callback=callback,
-                                    blocksize=1024,
-                                    finished_callback=event.set)
-            with stream:
-                event.wait()
+            def callback(outdata, frames, time, status):
+                data = wf.buffer_read(frames, dtype='float32')
+                if len(outdata) > len(data) or self.node.pause_flag.is_set():
+                    outdata[:len(data)] = data
+                    outdata[len(data):] = b'\x00' * (len(outdata) - len(data))
+                    raise sd.CallbackStop
+                else:
+                    outdata[:] = data
+
+                stream = sd.RawOutputStream(samplerate=wf.samplerate,
+                                        channels=wf.channels,
+                                        callback=callback,
+                                        blocksize=1024,
+                                        finished_callback=event.set)
+                with stream:
+                    event.wait()
